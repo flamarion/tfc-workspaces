ed264687257e37b47019186abccc07d7e208ab60
Flamarion Jorge <flamarion@hashicorp.com>
2021-01-04 09:33:31 +0100

    Fixing TF version in workspaces

--- tfe_aws_demo.tf
+++ tfe_aws_demo.tf
@@ -1,24 +1,9 @@
 module "tfe_aws_demo" {
-  source = "./modules"
-  name   = "tfe-aws-demo"
-  org    = var.tfc_org
+  source            = "./modules"
+  name              = "tfe-aws-demo"
+  org               = var.tfc_org
+  terraform_version = "0.14.3"
   ws_variables = {
-    access_key_id = {
-      key          = "AWS_ACCESS_KEY_ID"
-      value        = var.aws_access_key_id
-      category     = "env"
-      workspace_id = module.tfe_aws_demo.ws_id
-      description  = "AWS_ACCESS_KEY_ID"
-      sensitive    = true
-    },
-    secret_access_key = {
-      key          = "AWS_SECRET_ACCESS_KEY"
-      value        = var.aws_secret_access_key
-      category     = "env"
-      workspace_id = module.tfe_aws_demo.ws_id
-      description  = "AWS_SECRET_ACCESS_KEY"
-      sensitive    = true
-    },
     ssh_key = {
       key          = "cloud_pub"
       value        = var.pub_ssh_key
