c189a66488098da44f83e44f87d8461ec2b616c6
Flamarion Jorge <flamarion@hashicorp.com>
2021-01-19 10:38:07 +0100

    Adding Azure Vnet workspace

--- tf_azure_virtual_network.tf
+++ tf_azure_virtual_network.tf
@@ -1,6 +1,6 @@
-module "tf_azure_rg" {
+module "tf_azure_virtual_network" {
   source            = "./modules"
-  name              = "tf-azure-rg"
+  name              = "tf-azure-virtual-network"
   org               = var.tfc_org
   terraform_version = "0.14.4"
   ws_variables = {
@@ -8,7 +8,7 @@ module "tf_azure_rg" {
       key          = "ARM_TENANT_ID"
       value        = var.arm_tenant_id
       category     = "env"
-      workspace_id = module.tf_azure_rg.ws_id
+      workspace_id = module.tf_azure_virtual_network.ws_id
       description  = "AzureRM Tenant ID"
       sensitive    = true
     },
@@ -16,7 +16,7 @@ module "tf_azure_rg" {
       key          = "ARM_SUBSCRIPTION_ID"
       value        = var.arm_subscription_id
       category     = "env"
-      workspace_id = module.tf_azure_rg.ws_id
+      workspace_id = module.tf_azure_virtual_network.ws_id
       description  = "AzureRM Subscription ID"
       sensitive    = true
     },
@@ -24,7 +24,7 @@ module "tf_azure_rg" {
       key          = "ARM_CLIENT_ID"
       value        = var.arm_client_id
       category     = "env"
-      workspace_id = module.tf_azure_rg.ws_id
+      workspace_id = module.tf_azure_virtual_network.ws_id
       description  = "AzureRM Client ID"
       sensitive    = true
     },
@@ -32,7 +32,7 @@ module "tf_azure_rg" {
       key          = "ARM_CLIENT_SECRET"
       value        = var.arm_client_secret
       category     = "env"
-      workspace_id = module.tf_azure_rg.ws_id
+      workspace_id = module.tf_azure_virtual_network.ws_id
       description  = "AzureRM Client Secret"
       sensitive    = true
     }
